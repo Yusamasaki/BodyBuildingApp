fdd94a8e8e4dbfb072ddbac26c032d03275c894c
EC2 Default User <ec2-user@ip-172-31-38-175.ap-northeast-1.compute.internal>
2020-04-04 06:16:16 +0000

    モデル大移動前

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20200323203505) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "designated_work_start_time", default: "2020-04-02 01:00:00"
-    t.datetime "designated_work_end_time", default: "2020-04-02 10:00:00"
+    t.datetime "designated_work_start_time", default: "2020-04-04 01:00:00"
+    t.datetime "designated_work_end_time", default: "2020-04-04 10:00:00"
     t.datetime "expected_end_time"
     t.string "next_day"
     t.string "business_processing_contents"
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(version: 20200323203505) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-04-01 23:00:00"
-    t.datetime "work_time", default: "2020-04-01 22:30:00"
+    t.datetime "basic_time", default: "2020-04-03 23:00:00"
+    t.datetime "work_time", default: "2020-04-03 22:30:00"
     t.string "superior"
     t.integer "uid"
     t.integer "employee_number"
